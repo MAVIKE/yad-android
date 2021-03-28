fc36b8e1bd6654cc4ab0f05284c3a6a96e32e5fc
Vladimir Stepanenko <vovac12@gmail.com>
2021-03-28 15:44:16 +0300

    gh-1 First implementation of RepositoryProvider

--- yad_core/src/main/java/ru/m2d/yad_core/core/RepositoryProvider.java
+++ yad_core/src/main/java/ru/m2d/yad_core/core/RepositoryProvider.java
@@ -1,7 +1,12 @@
 package ru.m2d.yad_core.core;
 
 import ru.m2d.yad_core.core.repos.UsersRepo;
+import ru.m2d.yad_core.services.mock.repos.MockUsersRepo;
 
-public interface RepositoryProvider {
-    UsersRepo getUsersRepo();
+public class RepositoryProvider {
+    private static UsersRepo usersRepo = new MockUsersRepo();
+
+    UsersRepo getUsersRepo() {
+        return usersRepo;
+    }
 }
